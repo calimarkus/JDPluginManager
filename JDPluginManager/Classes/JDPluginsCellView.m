7355509a233e70891ab166afb3b3e989cb6cb720
Markus Emrich <Markus.Emrich@gmail.com>
2013-04-22 15:51:24 +0200

    labeling update

--- JDPluginManager/Classes/JDPluginsCellView.m
+++ JDPluginManager/Classes/JDPluginsCellView.m
@@ -12,9 +12,9 @@
 
 -(void)setCellWithPluginMetaData:(JDPluginMetaData *)pluginData isInstalledPlugin:(BOOL)isInstalledPlugin
 {
-    subTitleTextField.stringValue = pluginData.gitHubDescription ? pluginData.gitHubDescription : @"Loading...";
+    subTitleTextField.stringValue = pluginData.gitHubDescription ? pluginData.gitHubDescription : @"Loading Description from GitHub...";
     self.textField.stringValue = pluginData.name;
-    installUnInstallButton.title = isInstalledPlugin ? @"UnInstall" : @"Install";
+    installUnInstallButton.title = isInstalledPlugin ? @"Uninstall" : @"Install";
     revealInFinderButton.hidden = !isInstalledPlugin;
     lastPushDate.stringValue = pluginData.lastPushDate ? [pluginData.lastPushDate description] : @"";
     needsUpdateButton.hidden = !pluginData.needsUpdate;
