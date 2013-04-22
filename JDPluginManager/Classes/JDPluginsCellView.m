7b69050f3bb1d66d0b58dd768b337f55f561c5a1
Markus Emrich <Markus.Emrich@gmail.com>
2013-04-22 19:02:21 +0200

    removed debug log + commenting

--- JDPluginManager/Classes/JDPluginsCellView.m
+++ JDPluginManager/Classes/JDPluginsCellView.m
@@ -13,8 +13,10 @@
 -(void)setCellWithPluginMetaData:(JDPluginMetaData*)pluginData
                isInstalledPlugin:(BOOL)isInstalledPlugin
 {
+    // name
     self.textField.stringValue = pluginData.name;
     
+    // description
     if (pluginData.gitHubDescription.length > 0) {
         subTitleTextField.stringValue = pluginData.gitHubDescription;
     } else if (!pluginData.gitHubDescription) {
@@ -23,13 +25,16 @@
         subTitleTextField.stringValue = @"No Description";
     }
     
-    installUnInstallButton.title  = isInstalledPlugin ? @"Uninstall" : @"Install";
-    revealInFinderButton.hidden = !isInstalledPlugin;
+    // date
     lastPushDate.stringValue = pluginData.lastPushDate ? [pluginData.lastPushDate description] : @"";
+
+    // install button
+    installUnInstallButton.title  = isInstalledPlugin ? @"Uninstall" : @"Install";
+
+    // button hiding
     needsUpdateButton.hidden = !pluginData.needsUpdate;
+    revealInFinderButton.hidden = !isInstalledPlugin;
     readmeButton.hidden = !isInstalledPlugin;
-    
-    NSLog(@"%@ / %@", pluginData.name, pluginData.gitHubDescription);
 }
 
 @end
